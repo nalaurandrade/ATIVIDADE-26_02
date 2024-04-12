9070b16fe50cba7d49ebd8f7b72992485420ab4a
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-04-12 16:01:47 -0300

    Add files via upload

---
+++ po 26-02/po 26-02/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faturamento
+{
+    internal class Cliente
+    {
+        public string Nome;
+        public string Cpf;
+        public string Status;
+    }
+}
---
+++ po 26-02/po 26-02/Program.cs
@@ -0,0 +1,19 @@
+﻿using Faturamento;
+using System;
+
+Cliente _cliente;
+_cliente = new Cliente();
+
+_cliente.Nome = "João";
+_cliente.Cpf = "56646";
+_cliente.Status = "Ativo";
+
+Console.Write(_cliente.Nome);
+
+Console.Read();
+
+Cliente _cliente2 = new Cliente();
+
+_cliente2 = _cliente;
+
+Console.Write(_cliente2.Nome);
